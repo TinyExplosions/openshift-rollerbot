c0aedbabbacd0ad11229a8553ab999878643aba0
TinyExplosions <agraham@serenos.net>
2016-07-12 08:07:55 +0100

    testing changing to express

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+const http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    contentTypes = require('./utils/content-types'),
+    sysInfo = require('./utils/sys-info'),
+    express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    env = process.env;
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+var app = express();
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+// Enable CORS for all requests
+app.use(cors());
+app.use(bodyParser());
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url.indexOf('/info/') == 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
+
+// allow serving of static files from the public directory
+app.use(express.static(__dirname + '/static'));
+
+app.get('/health', function(req, res){
+  res.send(1);
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
+app.get('/info', function(req, res){
+  res.send(JSON.stringify(sysInfo[url.slice(6)]()));
+});
+
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
+    console.log(`Application worker ${process.pid} started...`);
 });
