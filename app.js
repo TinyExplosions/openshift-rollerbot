85ec077f7b5089f1f5eb574b5b759610189556a3
TinyExplosions <agraham@serenos.net>
2016-07-12 08:28:05 +0100

    testing page generation

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
+process.env.SLACK_VERIFY_TOKEN = "TEST";
+process.env.SLACK_BOT_COMMAND = "TEST";
+process.env.SLACK_CLIENT_ID = "TEST";
+process.env.SLACK_CLIENT_SECRET = "TEST";
+
 const http = require('http'),
     fs = require('fs'),
     path = require('path'),
     contentTypes = require('./utils/content-types'),
     sysInfo = require('./utils/sys-info'),
     express = require('express'),
-    cors = require('cors'),
+    // cors = require('cors'),
     bodyParser = require('body-parser'),
     env = process.env;
 
 var app = express();
 
-// Enable CORS for all requests
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser());
 
+app.use('/', require('./lib/signupPage')());
+app.use('/slackbot', require('./lib/slackbot')());
 
-// allow serving of static files from the public directory
-app.use(express.static(__dirname + '/static'));
-
+// health endpoint is mandatory for openshift gears
 app.get('/health', function(req, res){
   res.send(1);
 });
-
-app.get('/info', function(req, res){
-  res.send(JSON.stringify(sysInfo.gen()));
-});
-
 
 
 app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
